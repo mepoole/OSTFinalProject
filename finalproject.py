cf3b23c303ac00c2563a088ad2975262cace88d0
mepoole <marthaepoole@gmail.com>
2014-12-02 22:00:21 -0500

    adding Django Template and datastore models

--- finalproject.py
+++ finalproject.py
@@ -1,10 +1,47 @@
+import cgi
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import os
+from google.appengine.ext.webapp import template
 import webapp2
+
+class Question(ndb.Model):
+    author = ndb.UserProperty()
+    title = ndb.StringProperty(indexed=False)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    
+class Answer(ndb.Model):
+    author = ndb.UserProperty()
+    title = ndb.StringProperty(indexed=False)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class Vote(ndb.Model):
+    author = ndb.UserProperty()
+    vote = ndb.BooleanProperty()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+        
+        template_values = {
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+        
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+        
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/NewQuestion', NewQuestion),
 ], debug=True)
