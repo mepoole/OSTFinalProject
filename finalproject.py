76d94dc5d9b8a77b4639df1efe0657f28ca5dc99
mepoole <marthaepoole@gmail.com>
2014-12-03 21:27:01 -0500

    added form submission and some additional stuff

--- finalproject.py
+++ finalproject.py
@@ -1,16 +1,22 @@
 import cgi
 import urllib
-
+import json
 from google.appengine.api import users
 from google.appengine.ext import ndb
 import os
 from google.appengine.ext.webapp import template
 import webapp2
 
+DEFAULT_FORUM_NAME = 'mep505'
+
+def forum_key(forum_name=DEFAULT_FORUM_NAME):
+    return ndb.Key('forum', forum_name)
+
 class Question(ndb.Model):
     author = ndb.UserProperty()
     title = ndb.StringProperty(indexed=False)
     content = ndb.StringProperty(indexed=False)
+    tags = ndb.JsonProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
     
 class Answer(ndb.Model):
@@ -23,8 +29,23 @@ class Vote(ndb.Model):
     author = ndb.UserProperty()
     vote = ndb.BooleanProperty()
 
+class NewQuestion (webapp2.RequestHandler):
+    def post(self):
+        if users.get_current_user():
+            question=Question(parent=forum_key())
+            question.content = self.request.get('content')
+            question.author=users.get_current_user()
+            question.title=self.request.get('title')
+            question.put()
+        self.redirect('/')
+
+class QuestionDetail(webapp2.RequestHandler):
+    def get(self):
+        question=self.request.get('question')
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        questions = Question.query(ancestor=forum_key()).order(-Question.date)
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
@@ -33,6 +54,8 @@ class MainPage(webapp2.RequestHandler):
             url_linktext = 'Login'
         
         template_values = {
+            'user' : users.get_current_user(),
+            'questions' : questions,
             'url': url,
             'url_linktext': url_linktext,
         }
@@ -44,4 +67,5 @@ class MainPage(webapp2.RequestHandler):
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/NewQuestion', NewQuestion),
+    ('/QuestionDetail', QuestionDetail),
 ], debug=True)
