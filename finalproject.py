1ce2685bdefa28128c141d38109c11fb64883b24
mepoole <marthaepoole@gmail.com>
2014-12-05 18:32:20 -0500

    added pagination, working on images

--- finalproject.py
+++ finalproject.py
@@ -17,12 +17,14 @@ class Question(ndb.Model):
     title = ndb.StringProperty()
     content = ndb.StringProperty(indexed=False)
     tags = ndb.JsonProperty()
+    image = ndb.BlobProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
     
 class Answer(ndb.Model):
     author = ndb.UserProperty()
     title = ndb.StringProperty()
     content = ndb.StringProperty(indexed=False)
+    image = ndb.BlobProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 class Vote(ndb.Model):
@@ -37,6 +39,7 @@ class NewQuestion (webapp2.RequestHandler):
             question.author=users.get_current_user()
             question.title=self.request.get('title')
             question.tags=self.request.get('tags', allow_multiple=True)
+            question.image=str(self.request.get('image'))
             question.put()
         self.redirect('/')
 
@@ -52,12 +55,30 @@ class QuestionDetail(webapp2.RequestHandler):
         
         path = os.path.join(os.path.dirname(__file__), 'QuestionDetail.html')
         self.response.out.write(template.render(path, template_values))
-        
+
+class Image(webapp2.RequestHandler):
+        def get(self):
+            questionKey=self.request.get('questionKey')
+            question = ndb.Key(urlsafe=questionKey).get()
+            if question.image:
+                self.response.headers['Content-Type'] = 'image/png'
+                self.response.out.write(question.image)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        offset=0
+        entriesPerPage=10
+        #variable to determine whether to add a "next" or "prev" link
+        nextLink=False
+        prevLink=False
+        if self.request.get('offset') and self.request.get('offset') != '0':
+            offset=int(self.request.get('offset'))
+            prevLink=True
         questions = Question.query(ancestor=forum_key()).order(-Question.date)
-        questions=questions.fetch(10, offset=0)
+        questions=questions.fetch(entriesPerPage+1, offset=offset)
+        if len(questions)==entriesPerPage+1:
+            offset=offset+entriesPerPage
+            nextLink=True
         #for question in questions:
             #question.key.delete()
         if users.get_current_user():
@@ -71,6 +92,9 @@ class MainPage(webapp2.RequestHandler):
             'user' : users.get_current_user(),
             'questions' : questions,
             'url': url,
+            'offset': offset,
+            'nextLink': nextLink,
+            'prevLink' : prevLink,
             'url_linktext': url_linktext,
         }
         
@@ -82,4 +106,5 @@ application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/NewQuestion', NewQuestion),
     ('/QuestionDetail', QuestionDetail),
+    ('/Image', Image),
 ], debug=True)
