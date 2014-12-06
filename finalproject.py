291f921951f20a8a37b24df367b79c2c4bef1520
mepoole <marthaepoole@gmail.com>
2014-12-06 13:16:45 -0500

    added images with blobstore

--- finalproject.py
+++ finalproject.py
@@ -4,20 +4,26 @@ import json
 from google.appengine.api import users
 from google.appengine.ext import ndb
 import os
+from google.appengine.api import images
 from google.appengine.ext.webapp import template
 import webapp2
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+
 
 DEFAULT_FORUM_NAME = 'mep505'
 
 def forum_key(forum_name=DEFAULT_FORUM_NAME):
     return ndb.Key('forum', forum_name)
+    
 
 class Question(ndb.Model):
     author = ndb.UserProperty()
     title = ndb.StringProperty()
     content = ndb.StringProperty(indexed=False)
     tags = ndb.JsonProperty()
-    image = ndb.BlobProperty()
+    image = ndb.BlobKeyProperty()
+    imageURL = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
     
 class Answer(ndb.Model):
@@ -30,18 +36,6 @@ class Answer(ndb.Model):
 class Vote(ndb.Model):
     author = ndb.UserProperty()
     vote = ndb.BooleanProperty()
-
-class NewQuestion (webapp2.RequestHandler):
-    def post(self):
-        if users.get_current_user():
-            question=Question(parent=forum_key())
-            question.content = self.request.get('content')
-            question.author=users.get_current_user()
-            question.title=self.request.get('title')
-            question.tags=self.request.get('tags', allow_multiple=True)
-            question.image=str(self.request.get('image'))
-            question.put()
-        self.redirect('/')
 
 class QuestionDetail(webapp2.RequestHandler):
     def get(self):
@@ -56,16 +50,24 @@ class QuestionDetail(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'QuestionDetail.html')
         self.response.out.write(template.render(path, template_values))
 
-class Image(webapp2.RequestHandler):
-        def get(self):
-            questionKey=self.request.get('questionKey')
-            question = ndb.Key(urlsafe=questionKey).get()
-            if question.image:
-                self.response.headers['Content-Type'] = 'image/png'
-                self.response.out.write(question.image)
+                
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  def post(self):
+    question=Question(parent=forum_key())
+    question.content = self.request.get('content')
+    question.author=users.get_current_user()
+    question.title=self.request.get('title')
+    question.tags=self.request.get('tags', allow_multiple=True)
+    upload_files = self.get_uploads('image')  # 'file' is file upload field in the form
+    blob_info = upload_files[0]
+    question.image=blob_info.key()
+    question.imageURL = images.get_serving_url(blob_info.key())
+    question.put()
+    self.redirect('/')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        upload_url = blobstore.create_upload_url('/upload')
         offset=0
         entriesPerPage=10
         #variable to determine whether to add a "next" or "prev" link
@@ -91,11 +93,12 @@ class MainPage(webapp2.RequestHandler):
         template_values = {
             'user' : users.get_current_user(),
             'questions' : questions,
-            'url': url,
             'offset': offset,
             'nextLink': nextLink,
             'prevLink' : prevLink,
+            'url': url,
             'url_linktext': url_linktext,
+            'upload_url': upload_url,
         }
         
         path = os.path.join(os.path.dirname(__file__), 'index.html')
@@ -104,7 +107,6 @@ class MainPage(webapp2.RequestHandler):
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/NewQuestion', NewQuestion),
     ('/QuestionDetail', QuestionDetail),
-    ('/Image', Image),
+    ('/upload', UploadHandler),
 ], debug=True)
