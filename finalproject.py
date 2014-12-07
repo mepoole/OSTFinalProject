b9e8432f6be005a6ec6dcfba464fc5366a41e113
mepoole <marthaepoole@gmail.com>
2014-12-07 16:00:05 -0500

    added editing, started votinh

--- finalproject.py
+++ finalproject.py
@@ -24,7 +24,7 @@ class Question(ndb.Model):
     tags = ndb.JsonProperty()
     image = ndb.BlobKeyProperty()
     imageURL = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
+    date = ndb.DateTimeProperty(auto_now=True)
     
 class Answer(ndb.Model):
     author = ndb.UserProperty()
@@ -32,10 +32,11 @@ class Answer(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     image = ndb.BlobKeyProperty()
     imageURL = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
+    date = ndb.DateTimeProperty(auto_now=True)
 
 class Vote(ndb.Model):
     author = ndb.UserProperty()
+    #true if an up vote, false if a down vote
     vote = ndb.BooleanProperty()
 
 class QuestionDetail(webapp2.RequestHandler):
@@ -92,6 +93,7 @@ class Edit(webapp2.RequestHandler):
         self.response.out.write(template.render(path, template_values))
     
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    #handles all question and answer creations and edits
   def post(self):
     if self.request.get('form') == 'question':
         question=Question(parent=forum_key())
@@ -119,10 +121,64 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
         answer.put()
         redirectURL = "/QuestionActivity?questionKey=%s" % self.request.get('questionKey')
         self.redirect(redirectURL)
+    elif self.request.get('form') == 'editAnswer':
+        answerKey=ndb.Key(urlsafe=self.request.get('key'))
+        answer=answerKey.get()
+        answer.content = self.request.get('content')
+        answer.title=self.request.get('title')
+        upload_files = self.get_uploads('image')
+        if upload_files:
+            blob_info = upload_files[0]
+            answer.image=blob_info.key()
+            answer.imageURL = images.get_serving_url(blob_info.key())
+        answer.put()
+        redirectURL = "/QuestionActivity?questionKey=%s" % answerKey.parent().urlsafe()
+        self.redirect(redirectURL)
+    elif self.request.get('form') == 'editQuestion':
+        question=ndb.Key(urlsafe=self.request.get('key')).get()
+        question.content = self.request.get('content')
+        question.author=users.get_current_user()
+        question.title=self.request.get('title')
+        question.tags=self.request.get('tags', allow_multiple=True)
+        upload_files = self.get_uploads('image')
+        if upload_files:
+            blob_info = upload_files[0]
+            question.image=blob_info.key()
+            question.imageURL = images.get_serving_url(blob_info.key())
+        question.put()
+        self.redirect('/')
 
+class ProcessVote(webapp2.RequestHandler):
+    def post(self):
+        if self.request.get('key'):
+            #check to see if vote already exists
+            getVote = Vote.query(Vote.author==users.get_current_user(), ancestor=ndb.Key(urlsafe=self.request.get('key'))).get()         
+            if getVote:
+                if self.request.get('vote') == "up":
+                    getVote.vote=True
+                else:
+                    getVote.vote=False
+                getVote.put()
+                self.redirect('/')
+            else:
+                vote=Vote(parent=ndb.Key(urlsafe=self.request.get('key')))
+                vote.author=users.get_current_user()
+                if self.request.get('vote') == "up":
+                    vote.vote=True
+                else:
+                    vote.vote=False
+                vote.put()
+                self.redirect('/')
+            
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
         upload_url = blobstore.create_upload_url('/upload')
         offset=0
         entriesPerPage=10
@@ -137,18 +193,24 @@ class MainPage(webapp2.RequestHandler):
         if len(questions)==entriesPerPage+1:
             offset=offset+entriesPerPage
             nextLink=True
+        questionAndVotes= []
+        for question in questions:
+            totalUp=0
+            totalDown=0
+            votes = Vote.query(ancestor=question.key).fetch()
+            for vote in votes:
+                if vote.vote:
+                    totalUp += 1
+                else:
+                    totalDown += 1
+            questionAndVotes.append((question, totalUp, totalDown))
         #for question in questions:
             #question.key.delete()
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
         
         template_values = {
             'user' : users.get_current_user(),
             'questions' : questions,
+            'questionAndVotes' : questionAndVotes,
             'offset': offset,
             'nextLink': nextLink,
             'prevLink' : prevLink,
@@ -166,5 +228,6 @@ application = webapp2.WSGIApplication([
     ('/QuestionDetail', QuestionDetail),
     ('/upload', UploadHandler),
     ('/QuestionActivity', QuestionActivity),
-    ('/Edit', Edit)
+    ('/Edit', Edit),
+    ('/ProcessVote', ProcessVote)
 ], debug=True)
