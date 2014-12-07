10c908bb5bf6ec613c24e3b65490a3d497a321b3
mepoole <marthaepoole@gmail.com>
2014-12-07 13:22:24 -0500

    added answer functionality and started on editing for q/a

--- finalproject.py
+++ finalproject.py
@@ -30,7 +30,8 @@ class Answer(ndb.Model):
     author = ndb.UserProperty()
     title = ndb.StringProperty()
     content = ndb.StringProperty(indexed=False)
-    image = ndb.BlobProperty()
+    image = ndb.BlobKeyProperty()
+    imageURL = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 class Vote(ndb.Model):
@@ -50,20 +51,75 @@ class QuestionDetail(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'QuestionDetail.html')
         self.response.out.write(template.render(path, template_values))
 
-                
+class QuestionActivity(webapp2.RequestHandler):
+    def get(self):
+        if self.request.get('questionKey'):
+            upload_url = blobstore.create_upload_url('/upload')
+            questionKey=ndb.Key(urlsafe=self.request.get('questionKey'))
+            question=questionKey.get()
+            answers = Answer.query(ancestor=questionKey).order(-Answer.date)
+            if users.get_current_user():
+                url = users.create_logout_url(self.request.uri)
+                url_linktext = 'Logout'
+            else:
+                url = users.create_login_url(self.request.uri)
+                url_linktext = 'Login'
+            template_values = {
+            'user' : users.get_current_user(),
+            'question' : question,
+            'upload_url' : upload_url,
+            'answers' : answers,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+        
+        path = os.path.join(os.path.dirname(__file__), 'QuestionActivity.html')
+        self.response.out.write(template.render(path, template_values))
+
+class Edit(webapp2.RequestHandler):
+    def get(self):
+        if self.request.get('key'):
+            upload_url = blobstore.create_upload_url('/upload')
+            key=ndb.Key(urlsafe=self.request.get('key'))
+            item=key.get()
+            template_values = {
+            'user' : users.get_current_user(),
+            'upload_url' : upload_url,
+            'item' : item,
+            'type' : self.request.get('type'),
+        }
+        path = os.path.join(os.path.dirname(__file__), 'Edit.html')
+        self.response.out.write(template.render(path, template_values))
+    
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
   def post(self):
-    question=Question(parent=forum_key())
-    question.content = self.request.get('content')
-    question.author=users.get_current_user()
-    question.title=self.request.get('title')
-    question.tags=self.request.get('tags', allow_multiple=True)
-    upload_files = self.get_uploads('image')  # 'file' is file upload field in the form
-    blob_info = upload_files[0]
-    question.image=blob_info.key()
-    question.imageURL = images.get_serving_url(blob_info.key())
-    question.put()
-    self.redirect('/')
+    if self.request.get('form') == 'question':
+        question=Question(parent=forum_key())
+        question.content = self.request.get('content')
+        question.author=users.get_current_user()
+        question.title=self.request.get('title')
+        question.tags=self.request.get('tags', allow_multiple=True)
+        upload_files = self.get_uploads('image')
+        if upload_files:
+            blob_info = upload_files[0]
+            question.image=blob_info.key()
+            question.imageURL = images.get_serving_url(blob_info.key())
+        question.put()
+        self.redirect('/')
+    elif self.request.get('form') == 'answer':
+        answer=Answer(parent=ndb.Key(urlsafe=self.request.get('questionKey')))
+        answer.content = self.request.get('content')
+        answer.author=users.get_current_user()
+        answer.title=self.request.get('title')
+        upload_files = self.get_uploads('image')
+        if upload_files:
+            blob_info = upload_files[0]
+            answer.image=blob_info.key()
+            answer.imageURL = images.get_serving_url(blob_info.key())
+        answer.put()
+        redirectURL = "/QuestionActivity?questionKey=%s" % self.request.get('questionKey')
+        self.redirect(redirectURL)
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -109,4 +165,6 @@ application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/QuestionDetail', QuestionDetail),
     ('/upload', UploadHandler),
+    ('/QuestionActivity', QuestionActivity),
+    ('/Edit', Edit)
 ], debug=True)
